6aa21e237da5d10758dc782f30485161d19b0eb5
viniciussouza70 <viniciussouza70@gmail.com>
2019-07-11 23:52:21 -0300

    finalizada aula 1

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,11 +6,11 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => //o escopo de uma => é léxico, não dinâmico, logo n muda com o contexto da func.
+            this._negociacoesView.update(model));
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-        
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
@@ -21,7 +21,6 @@ class NegociacaoController {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
@@ -29,6 +28,13 @@ class NegociacaoController {
         this._limpaFormulario();   
     }
     
+    apaga() {
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+    }
+
     _criaNegociacao() {
         
         return new Negociacao(
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,28 @@
 class ListaNegociacoes {
     
-    constructor() {
+    constructor(armadilha) {
         
         this._negociacoes = [];
+        this._armadilha = armadilha;
+     
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
+        // //apply é um método estático de apply, que permite trocar o contexto do this. Primeiro, recebe o método que vc quer chamar, segundo qual vai ser o this da função e terceiro, os parâmetros, através de um array 
+        // Reflect.apply(this._armadilha, this._contexto, [this]);  
+        this._armadilha(this);
     }
     
     get negociacoes() {
         
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        // Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
 }
